3b1a13a340418914ba08b7d841c546373af750fc
Ivan Bobanović <ivanbobanovic13@gmail.com>
2026-01-22 20:19:31 +0100

    Fix: Use absolute path alias for globals.css import

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "../../globals.css";
+import "@/app/globals.css";
 import { Oswald, Outfit } from "next/font/google";
 import Link from "next/link";
 import { Header } from "@/components/Header";
